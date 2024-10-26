5884808d7b3830e191908fced519863276e96d8a
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-10-26 17:09:56 +0300

    add constants for redis and jwt

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,31 +1,56 @@
 /**
- * Hone server host
+ * Express server host
  */
-export const SERVER_HOST: string = process.env.SERVER_HOST || '127.0.0.1';
+export const SERVER_HOST: string = process.env.SERVER_HOST || "127.0.0.1";
 
 /**
- * hono server port
+ * Express server port
  */
-export const SERVER_PORT: string = process.env.SERVER_PORT || '5000';
+export const SERVER_PORT: string = process.env.SERVER_PORT || "8080";
 
 /**
  * Database host
  */
-export const DB_HOST: string = process.env.DB_HOST || '127.0.0.1';
+export const DB_HOST: string = process.env.DB_HOST || "127.0.0.1";
 
 /**
  * Database port
  */
-export const DB_PORT: string = process.env.DB_PORT || '27017';
+export const DB_PORT: string = process.env.DB_PORT || "27017";
 
 /**
  * Database name
  */
-export const DB_NAME: string = process.env.DB_NAME || 'orgdb';
+export const DB_NAME: string = process.env.DB_NAME || "orgdb";
 
 /**
- * the secret key for jwt token
+ * RedisDB port
  */
-export const JWT_SECRET_KEY: string =
-  process.env.JWT_SECRET_KEY || 'strong_secret_key';
+export const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
 
+/**
+ * RedisDB port
+ */
+export const REDIS_PORT = process.env.REDIS_PORT || '6379';
+
+/**
+ * the secret key for access token
+ */
+export const ACCESS_TOKEN_SECRET =
+	process.env.ACCESS_TOKEN_SECRET || "your-access-token-secret";
+
+/**
+ * the secret key for refresh token
+ */
+export const REFRESH_TOKEN_SECRET =
+	process.env.REFRESH_TOKEN_SECRET || "your-refresh-token-secret";
+
+/**
+ * expiration time of access token
+ */
+export const ACCESS_TOKEN_EXPIRES_IN = process.env.ACCESS_TOKEN_EXPIRES_IN || "15m";
+
+/**
+ * expiration time of access token
+ */
+export const REFRESH_TOKEN_EXPIRES_IN = process.env.REFRESH_TOKEN_EXPIRES_IN || "1d";
